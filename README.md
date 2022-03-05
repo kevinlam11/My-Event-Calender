9bdb37478c94fd75b2763aae2d859ff9b75abdcf
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-03-04 19:40:30 -0500

    Revised my script.js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # My-Event-Calender
+
+## My Event Calender
+
+[My-Event-Calender-Link](https://github.com/kevinlam11/My-Event-Calender)
+
+## Description
+
+I have created a calender that can log your events at a certain time of the day!
+
+## Screenshot
+
+![My Event Calender ](./Calender.png)
--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-var curr = moment().add(1, "hours").hour();
-moment().format("MMM Do YY");
+var rootEl = $("#root");
+var containerEl = $(".container");
 
-var currentDay = document.getElementById("currentDay");
-var currentTime = moment().format("LLL");
-currentDay.textContent += currentTime;
+var time = moment().add(1, "hours").hour();
+var replacement = moment().add(1, "hours").format("H A");
+$("#timeOfTheDay").text(moment().format("LLL"));
 
-$("#currentDay").text(moment().format("LLL"));
+function createTimeBlock(index) {
+  // Event Rows
+  var timeBlockEl = $("<div>");
+  timeBlockEl.attr("class", "row time-block");
+  timeBlockEl.attr("data-hour", index);
 
-$(".row").each(function () {
-  var hour = $(this).data("hour");
+  var hourBlockEl = $("<div>");
+  hourBlockEl.attr("class", "col-1 hour p-3");
+
+  var textAreaEl = $("<textarea>");
+  textAreaEl.attr("class", "col-10 description");
+
+  var iconEl = $("<i>");
+  iconEl.attr("class", "fas fa-save");
+
+  var saveBtnEl = $("<button>");
+  saveBtnEl.attr("class", "col-1 saveBtn");
+  saveBtnEl.append(iconEl);
+
+  // This is the complete time block element
+  timeBlockEl.append(hourBlockEl);
+  timeBlockEl.append(textAreaEl);
+  timeBlockEl.append(saveBtnEl);
+
+  return timeBlockEl;
+}
+
+function addTime() {}
+
+// Appending & Logic
+for (i = 1; i < 24; i++) {
+  var timeStamp = createTimeBlock(i);
+
+  var hour = timeStamp.data("hour");
   var message = localStorage.getItem(hour);
-  $(this.find(".description").text(message));
-  $this.addClass(hour > curr ? "future" : hour < curr ? "past" : "present");
-});
+  timeStamp.find(".description").text(message);
+  timeStamp.addClass(hour > time ? "future" : hour < time ? "past" : "present");
 
-$(".row").on("click", ".saveBtn", function () {
-  var result = $(this).parent().find(".descripion").val();
-  localStorage.setItem($(this).parent().data("hour"), result);
-});
+  timeStamp.find(".hour").text(hour);
+
+  timeStamp.on("click", ".saveBtn", function () {
+    var result = $(this).parent().find(".description").val();
+    localStorage.setItem($(this).parent().data("hour"), result);
+  });
+
+  containerEl.append(timeStamp);
+}
